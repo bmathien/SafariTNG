d6724095276dc76a56b6e3231b2c92660ce6a3ad
Brian Mathien <bmathien@barrington220.org>
2014-05-14 10:45:27 -0500

    Initial Commit

---
+++ SafariTNG/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SafariTNG
+//
+//  Created by teacher on 5/14/14.
+//  Copyright (c) 2014 Mathien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SafariTNG/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SafariTNG
+//
+//  Created by teacher on 5/14/14.
+//  Copyright (c) 2014 Mathien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
